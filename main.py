4c988530e6358a9d420500be066fe06f491fd904
Laurin Neff <laurin@laurinneff.ch>
2020-09-23 18:13:19 +0200

    Forgot to commit for a while. Updates: Listens for new comments on Reddit Checks if comment contains AMP links (implicit and explicit) If AMP links exist, it logs the reply it would make

--- main.py
+++ main.py
@@ -1,6 +1,64 @@
+import praw
 import utils
+import re
 
-comment_template = "Non-AMP version: {url}\n\
-\n\
-Beep Boop, I'm a bot. If you have any questions, my creator might check my messages.\n\
-By the way, I'm open source. You can find the code here: "
+comment_template = "I found some Google AMP links in your comment. Here are the normal links:\n\n\
+{links}\
+\n\n\
+Beep Boop, I'm a bot. If you have any questions, my creator might check my messages."
+link_regex = '\[(.+)\]\((https?:\/\/[\w\d./?=#%+&-]+)\)'
+implicit_link_regex = '(?<!\()https?:\/\/[\w\d./?=#%+&-]+(?!\))'
+
+reddit = praw.Reddit('anti-amp')
+
+subreddit = reddit.subreddit("all")
+
+
+def process_comments(comment):
+    links = re.findall(link_regex, comment.body)
+    # if not comment.author == '6b86b3ac03c167320d93':
+    # return
+    # print("{user}:\n{body}".format(user=comment.author, body=comment.body))
+
+    fixed_arr = []
+
+    if links:
+        for link in links:
+            fixed = process_link(comment, link)
+            if fixed:
+                fixed_arr.append(fixed)
+    links = re.findall(implicit_link_regex, comment.body)
+    if links:
+        for link in links:
+            fixed = process_link(comment, link, True)  # Implicit links show the URL as text
+            if fixed:
+                fixed_arr.append(fixed)
+
+    out = '- '
+    if fixed_arr:
+        out += '\n- '.join(fixed_arr)
+        print(f'Comment by {comment.author} with ID {comment.id} (https://reddit.com{comment.permalink})')
+        print(comment_template.format(links=out))
+
+
+def process_link(comment, link, implicit=False):
+    if not implicit:
+        text = link[0]
+        url = link[1]
+    else:
+        text = link
+        url = link
+    if utils.is_amp(url):
+        # print("link:", text, "to", url)
+        fixed = utils.amp_to_normal(url)
+        if implicit: text = fixed
+        return "[{text}]({fixed})".format(text=text, fixed=fixed)
+
+
+for comment in subreddit.stream.comments():
+    try:
+        process_comments(comment)
+    except Exception as e:
+        print('Error:', e)
+
+print(reddit)
--- utils.py
+++ utils.py
@@ -3,6 +3,22 @@ import requests
 from tld import get_tld
 
 
+def is_amp(url):
+    """
+    Check if the given URL is an AMP URL
+    :param url: The URL to check
+    :type url: string
+    :return: Returns a boolean if it's an AMP URL
+    :returns: bool
+    """
+    parsed = urlparse(url)
+
+    if get_tld(parsed.hostname, as_object=True, fix_protocol=True, fail_silently=True).domain == 'google' \
+            and parsed.path.startswith('/amp/'):
+        return True
+    return False
+
+
 def amp_to_normal(url):
     """
     Check if the given URL is an AMP url. If it is, send a request to find the normal URL
@@ -12,11 +28,8 @@ def amp_to_normal(url):
     :return: Returns the non-AMP version of the given URL if it's an AMP URL. Otherwise, it returns None
     :returns: string?
     """
-    parsed = urlparse(url)
-
-    if get_tld(parsed.hostname, as_object=True, fix_protocol=True).domain == 'google' and parsed.path.startswith('/amp/'):
+    if is_amp(url):
         r = requests.get(url)
-        if r.url != url:
-            return r.url
+        return r.url
     else:
         return None
